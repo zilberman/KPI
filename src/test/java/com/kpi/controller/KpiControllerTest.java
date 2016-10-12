2a2ba867655ed3bdd5f3b74bb1283f6656361e6f
Alexey.Kalugin <kalugin.alexey@gmail.com>
2016-10-12 19:31:15 +0300

    Were added unit tests.

---
+++ src/test/java/com/kpi/controller/KpiControllerTest.java
@@ -0,0 +1,41 @@
+package com.kpi.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import org.mockito.MockitoAnnotations;
+
+
+import com.kpi.controller.KpiController;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = { "/test-context.xml" })
+public class KpiControllerTest {
+    @Autowired
+    private KpiController kpiController;
+
+    private MockMvc       mockMvc;
+
+    @Before
+    public void setup() {
+        MockitoAnnotations.initMocks(this);
+        this.mockMvc = MockMvcBuilders.standaloneSetup(this.kpiController).build();
+    }
+
+    @Test
+    public void testMainPage() throws Exception {
+        this.mockMvc.perform(MockMvcRequestBuilders.get("/main"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.view().name("kpi"))
+                .andExpect(MockMvcResultMatchers.model().attributeExists("menuItems"));
+    }
+}
---
+++ src/test/java/com/kpi/service/MenuItemServiceTest.java
@@ -0,0 +1,45 @@
+package com.kpi.service;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.mockito.Spy;
+
+import com.kpi.service.MenuItemService;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = { "/test-context.xml" })
+public class MenuItemServiceTest {
+
+    @Spy
+    MenuItemService menuItemService;
+
+    @Before
+    public void setup() {
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    public void testMenuItemService() {
+        Assert.assertEquals(4, this.menuItemService.getMenuItems().size());
+    }
+
+    @Test
+    public void testMenuItemServiceAsSpy() {
+        final Map<String, String> menuItems = new HashMap<>();
+        menuItems.put("menu1", "content");
+
+        Mockito.when(this.menuItemService.getMenuItems()).thenReturn(menuItems);
+        Assert.assertEquals(1, this.menuItemService.getMenuItems().size());
+    }
+}
