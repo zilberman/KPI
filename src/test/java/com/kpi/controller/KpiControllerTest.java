40d936465d5a2ce5f45799bbcdc6a27756d3c7ad
Alexey.Kalugin <kalugin.alexey@gmail.com>
2016-10-13 15:06:04 +0300

    Were made changes to load configuration files for tests from /src/test/resources folder.

--- src/test/java/com/kpi/controller/KpiControllerTest.java
+++ src/test/java/com/kpi/controller/KpiControllerTest.java
@@ -18,7 +18,7 @@ import org.mockito.MockitoAnnotations;
 import com.kpi.controller.KpiController;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations={"classpath*:test-context.xml"})
+@ContextConfiguration(locations={"classpath:test-context.xml"})
 public class KpiControllerTest {
     @Autowired
     private KpiController kpiController;
