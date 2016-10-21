cb8cd37f9edae578e186229945be04d1737d87b6
Alexey.Kalugin <kalugin.alexey@gmail.com>
2016-10-21 14:22:54 +0300

    Was added ability to load projects into view via KpiController. Were added changes to run tests succesfully.

--- src/main/java/com/kpi/controller/KpiController.java
+++ src/main/java/com/kpi/controller/KpiController.java
@@ -1,23 +1,34 @@
 package com.kpi.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.kpi.model.Project;
 import com.kpi.service.MenuItemService;
+import com.kpi.service.ProjectService;
 
 @Controller
 public class KpiController {
     @Autowired
     private MenuItemService menuItemService;
 
+    @Autowired
+    private ProjectService  projectService;
+
     @RequestMapping(value = "/main", method = RequestMethod.GET)
     public ModelAndView menu() {
         final ModelAndView model = new ModelAndView("kpi");
 
+        final List<Project> projects = this.projectService.getAll();
+
         model.addObject("menuItems", this.menuItemService.getMenuItems());
+
+        model.addObject("projects", projects);
 
         return model;
     }
