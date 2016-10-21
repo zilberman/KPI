0deff577b040823550a0a7f59d0a3c19148fdd6c
Alexey.Kalugin <kalugin.alexey@gmail.com>
2016-10-21 13:19:31 +0300

    Were added ProjectService and its implementation.

---
+++ src/main/java/com/kpi/service/impl/ProjectServiceImpl.java
@@ -0,0 +1,36 @@
+package com.kpi.service.impl;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.kpi.dao.ProjectDao;
+import com.kpi.model.Project;
+import com.kpi.service.ProjectService;
+
+@Service("projectService")
+public class ProjectServiceImpl implements ProjectService {
+    @Autowired
+    private ProjectDao projectDao;
+
+    @Override
+    public void create(final Project project) {
+        this.projectDao.create(project);
+    }
+
+    @Override
+    public void delete(final Project project) {
+        this.projectDao.delete(project);
+    }
+
+    @Override
+    public List<Project> getAll() {
+        return this.projectDao.getAll();
+    }
+
+    @Override
+    public void update(final Project project) {
+        this.projectDao.update(project);
+    }
+}
