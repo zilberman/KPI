2681502b2837ee5ba3f9b508bbda1ee7578fbfab
Alexey.Kalugin <kalugin.alexey@gmail.com>
2016-11-01 18:15:32 +0300

    Database properties were moved into separate files both for test and dev environments.

--- src/test/java/com/kpi/controller/KpiControllerTest.java
+++ src/test/java/com/kpi/controller/KpiControllerTest.java
@@ -1,5 +1,9 @@
 package com.kpi.controller;
 
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
@@ -8,22 +12,13 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-
-import org.mockito.MockitoAnnotations;
-
-
-import com.kpi.controller.KpiController;
-
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations={"classpath:test-context.xml"})
+@ContextConfiguration(locations = { "classpath:test-context.xml" })
 public class KpiControllerTest {
     @Autowired
     private KpiController kpiController;
 
-    private MockMvc mockMvc;
+    private MockMvc       mockMvc;
 
     @Before
     public void setup() {
